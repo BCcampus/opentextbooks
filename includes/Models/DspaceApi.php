a36b04d19966402b374ec36852c25849e32b1743
bdolor <bpayne@bccampus.ca>
2017-05-29 14:27:18 -0700

    adding class, implementing rest interfaces, stubs

---
+++ includes/Models/DspaceApi.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Project: opentextbooks
+ * Project Sponsor: BCcampus <https://bccampus.ca>
+ * Copyright 2012-2017 Brad Payne <https://bradpayne.ca>
+ * Date: 2017-05-29
+ * Licensed under GPLv3, or any later version
+ *
+ * @author Brad Payne
+ * @package OPENTEXTBOOKS
+ * @license https://www.gnu.org/licenses/gpl-3.0.txt
+ * @copyright (c) 2012-2017, Brad Payne
+ */
+
+namespace BCcampus\OpenTextBooks\Models;
+
+use BCcampus\OpenTextBooks\Polymorphism;
+
+
+class DspaceApi implements Polymorphism\RestInterface {
+
+	function retrieve( $args ) {
+		// TODO: Implement retrieve() method.
+	}
+
+	function create() {
+		// TODO: Implement create() method.
+	}
+
+	function replace() {
+		// TODO: Implement replace() method.
+	}
+
+	function remove() {
+		// TODO: Implement remove() method.
+	}
+}
