30fb1b02963b9d7e21cb8c997fc500b217990c3a
bdolor <bpayne@bccampus.ca>
2017-05-29 15:44:42 -0700

    adding dspacebooks class, implementing stubs

---
+++ includes/Models/DspaceBooks.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Project: opentextbooks
+ * Project Sponsor: BCcampus <https://bccampus.ca>
+ * Copyright 2012-2017 Brad Payne <https://bradpayne.ca>
+ * Date: 2017-05-29
+ * Licensed under GPLv3, or any later version
+ *
+ * @author Brad Payne
+ * @package OPENTEXTBOOKS
+ * @license https://www.gnu.org/licenses/gpl-3.0.txt
+ * @copyright (c) 2012-2017, Brad Payne
+ */
+
+namespace BCcampus\OpenTextBooks\Models;
+
+use BCcampus\OpenTextBooks\Polymorphism;
+
+
+class DspaceBooks extends Polymorphism\DataAbstract {
+
+	/**
+	 * DspaceBooks constructor.
+	 *
+	 * @param Polymorphism\RestInterface $api
+	 * @param $args
+	 */
+	public function __construct( Polymorphism\RestInterface $api, $args ) {
+		// TODO: Implement constructor
+	}
+
+	/**
+	 * @return mixed
+	 */
+	function getResponses() {
+		// TODO: Implement getResponses() method.
+	}
+}
