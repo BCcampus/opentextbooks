23a0e4f6084d5fa891af96a2250525ee242c705f
bdolor <bpayne@bccampus.ca>
2018-02-09 15:42:31 -0800

    autoloader edge case for symlinked wp environment

--- config/environments/env.sample.php
+++ config/environments/env.sample.php
@@ -1,18 +1,4 @@
 <?php
-/**
- * Project: opentextbooks
- * Project Sponsor: BCcampus <https://bccampus.ca>
- * Copyright 2012-2016 Brad Payne <https://bradpayne.ca>
- * Date: 2016-05-31
- * Licensed under GPLv3, or any later version
- *
- * @author Brad Payne
- * @package OPENTEXTBOOKS
- * @license https://www.gnu.org/licenses/gpl-3.0.txt
- * @copyright (c) 2012-2016, Brad Payne
- *
- * Change the name of this file to `.env.mydomain.php`
- */
 
 return [
 	/*
