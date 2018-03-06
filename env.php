901208384c27978af289306c3a39ffa388a8dda7
bdolor <bpayne@bccampus.ca>
2018-03-06 12:19:05 -0800

    update documentation

--- env.php
+++ env.php
@@ -2,15 +2,15 @@
 /**
  * You can override which config file gets loaded (ideal for development)
  * otherwise the default is to get the value from $_SERVER['HTTP_HOST']
- * leve this blank for default behaviour (ie environment => '')
+ * leave this blank for default behaviour (ie environment => '')
  *
  * set the value of 'environment' equal to the domain of the application
- * create a corresponding .env.mydomain.php file in `config/environments`
+ * create a corresponding .env.mydomain.com.php file in `config/environments`
  *
  * example:
  *
- * set      'environment' => 'mydomain'
- * create   config/environment/.env.mydomain.php
+ * set      'environment' => 'mydomain.com'
+ * create   config/environment/.env.mydomain.com.php
  *
  */
 return [
