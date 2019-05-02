d0de2e54939d29982b5975e75f43b6d2e4ac09a6
alex-418 <aparedes@bccampus.ca>
2019-05-02 15:51:38 -0700

    resolve phpcs errors, applies coding standards

--- assets/templates/partial/menu.php
+++ assets/templates/partial/menu.php
@@ -1,25 +1,24 @@
-<?php $alpha = true;
-	$summary = false;
-	$subject_areas = \BCcampus\OpenTextBooks\Controllers\Catalogue\Otb::getSubjectStats($summary,$alpha);
+<?php $alpha       = true;
+	$summary       = false;
+	$subject_areas = \BCcampus\OpenTextBooks\Controllers\Catalogue\Otb::getSubjectStats( $summary, $alpha );
 ?>
 <div class="accordion mt-2" id="catalogue-menu">
-    <ul class="accordion-group list-unstyled">
-        <div class="accordion-group">
-            <li class="accordion-heading"><a class="accordion-toggle" href="?subject=">All Subjects</a></li>
-        </div>
-		<?php foreach($subject_areas as $sub1=>$sub2){ ?>
-            <div class="accordion-group">
-				<?php ($abr=substr($sub1,0,4)) ?>
-                <li class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse"
-                                                 data-parent="#catalogue-menu" href="#collapse<?php echo($abr) ?>"><?php echo($sub1)?></a></li>
-                <div id="collapse<?php echo($abr)?>" class="accordion-body collapse">
-                    <ul class="children">
-						<?php foreach($sub2 as $k=>$num) { ?>
-                            <li><a href="?subject=<?php echo($k) ?>"><?php echo($k) ?></a></li>
-						<?php }?>
-                    </ul>
-                </div>
-            </div>
+	<ul class="accordion-group list-unstyled">
+		<div class="accordion-group">
+			<li class="accordion-heading"><a class="accordion-toggle" href="?subject=">All Subjects</a></li>
+		</div>
+		<?php foreach ( $subject_areas as $sub1 => $sub2 ) { ?>
+			<div class="accordion-group">
+				<?php $abr = substr( $sub1, 0, 4 ) ?>
+				<li class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#catalogue-menu" href="#collapse<?php echo( $abr ) ?>"><?php echo( $sub1 ) ?></a></li>
+				<div id="collapse<?php echo( $abr ) ?>" class="accordion-body collapse">
+					<ul class="children">
+						<?php foreach ( $sub2 as $k => $num ) { ?>
+							<li><a href="?subject=<?php echo( $k ) ?>"><?php echo( $k ) ?></a></li>
+						<?php } ?>
+					</ul>
+				</div>
+			</div>
 		<?php } ?>
-    </ul>
+	</ul>
 </div>
